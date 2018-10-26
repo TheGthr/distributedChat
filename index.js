2073e8337c3887f18d35b1b78c710e83c682b0b1
TheGthr <theogauthier13@gmail.com>
2018-10-26 16:09:03 +0200

    Basic chat v0.1 fix

---
+++ index.js
@@ -0,0 +1,34 @@
+
+const express = require('express');
+const app = express();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+const port = 3000;
+var people = {};
+var nbPpl = 0;
+
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', function (req, res) {
+    res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', function (socket) {
+    socket.on('disconnect', function () {
+        nbPpl -= 1;
+        console.log('a user disconnected, nb ppl: ' + nbPpl);
+    });
+    socket.on('chat message', function (msg) {
+        console.log("message: " + msg);
+        io.emit('chat message', msg);
+    });
+});
+
+io.on('connection', (socket) => {
+    nbPpl += 1;
+    console.log('a user connected, nb ppl: ' + nbPpl);
+});
+
+http.listen(port, function () {
+    console.log('listening on localhost:' + port);
+});
