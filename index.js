336394c9997c772295bee02d56d126df10fa3d0f
TheGthr <theogauthier13@gmail.com>
2018-10-28 23:12:08 +0100

    Added BS and pseudo func

--- index.js
+++ index.js
@@ -1,34 +1,35 @@
 
-const express = require('express');
+const express = require("express");
 const app = express();
-const http = require('http').Server(app);
-const io = require('socket.io')(http);
+const http = require("http").Server(app);
+const io = require("socket.io")(http);
 const port = 3000;
-var people = {};
-var nbPpl = 0;
+let people = {};
+let nbPpl = 0;
+let connected = false;
 
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + "/public"));
 
-app.get('/', function (req, res) {
-    res.sendFile(__dirname + '/index.html');
+app.get("/", function (req, res) {
+    res.sendFile(__dirname + "/index.html");
 });
 
-io.on('connection', function (socket) {
-    socket.on('disconnect', function () {
+io.on("connection", function (socket) {
+    socket.on("disconnect", function () {
         nbPpl -= 1;
-        console.log('a user disconnected, nb ppl: ' + nbPpl);
+        console.log("a user disconnected, nb ppl: " + nbPpl);
     });
-    socket.on('chat message', function (msg) {
+    socket.on("chat message", function (msg) {
         console.log("message: " + msg);
-        io.emit('chat message', msg);
+        io.emit("chat message", msg);
     });
 });
 
-io.on('connection', (socket) => {
+io.on("connection", (socket) => {
     nbPpl += 1;
-    console.log('a user connected, nb ppl: ' + nbPpl);
+    console.log("a user connected, nb ppl: " + nbPpl);
 });
 
 http.listen(port, function () {
-    console.log('listening on localhost:' + port);
+    console.log("listening on localhost:" + port);
 });
